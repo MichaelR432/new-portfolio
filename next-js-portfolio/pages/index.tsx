0575feb30278eccb8f6a477f652d1a73a42a52d0
Michael Ramos <michaelaramos182@gmail.com>
2023-02-11 00:11:42 -0600

    fixed errors added card images

--- next-js-portfolio/pages/index.tsx
+++ next-js-portfolio/pages/index.tsx
@@ -4,12 +4,18 @@ import {BsFillMoonStarsFill} from 'react-icons/bs';
 import {AiFillLinkedin, AiFillGithub} from 'react-icons/ai';
 import Image from 'next/image';
 import mars from "../public/marsAvatarLg.png"
+import weather from "../public/weather.png"
+import passwordgen from "../public/passwordgen.png"
+import teamgen from "../public/teamgen.png"
+import { useState } from 'react';
 
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [darkMode, setDarkMode] =useState(false);
   return (
+    <div className={darkMode ? "dark" :""}>
     <>
       <Head>
         <title>Michael A Ramos Portfolio</title>
@@ -17,12 +23,15 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className=' bg-white px-10 md:px-20 lg:px-40'>
+      <main className=' bg-white dark:bg-slate-500 px-10 md:px-20 lg:px-40'>
         <section className='min-h-screen'>
           <nav className='py-10 mb-12 flex justify-between'>
             <h1 className='text-xl'>Developed By Mike</h1>
             <ul className='flex items-center'>
-              <li><BsFillMoonStarsFill className='cursor-pointer text-2xl'/></li>
+              <li>
+                <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} 
+                className='cursor-pointer text-2xl'/>
+              </li>
               <li>
                 <a className=' bg-gradient-to-r from-green-700 to-green-500 text-white px-4 py-2 rounded-md ml-8' href='#'>Resume</a>
               </li>
@@ -30,9 +39,11 @@ export default function Home() {
             </ul>
           </nav>
           <div className='text-center p-10'>
-            <h2 className=' text-5xl py-2 text-green-700 font-medium'>Michael A Ramos</h2>
-            <h3 className=' text-2xl py-2'>Full Stack Developer</h3>
-            <p className='text-md py-5 leading-8 text-gray-800'>Coding coder who likes to code!</p>
+            <h2 className=' text-5xl py-2 text-green-700 font-medium md:text-6xl'>Michael A Ramos</h2>
+            <h3 className=' text-2xl py-2 md:text-3xl'>Full Stack Developer</h3>
+              <p className='text-md py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto'>
+              Coding coder who likes to code! Coding coder who likes to code! Coding coder who likes to code!
+              </p>
           </div>
           <div className='text-5xl flex justify-center gap-16'>
             <AiFillLinkedin />
@@ -40,23 +51,36 @@ export default function Home() {
           </div>
           <div className='flex items-center mx-auto bg-gradient-to-b from-green-700 rounded-full w-80 h-80 mt-20'>
             <div className='mb-20'>
-            <Image src={mars} />
+             <Image alt='' src={mars} />
             </div>
           </div>
         </section>
 
         <section>
           <div>
-            <h3 className='text-3xl py-1 mt-5'>Something something</h3>
-            <p className='text-md py-2 leading-8 text-gray-800'> looking for a yab</p>
-            <p className='text-md py-2 leading-8 text-gray-800'>I know how to code</p>
+            <h3 className='text-center text-3xl py-1 mt-5'>Something something</h3>
+            <p className='text-center text-md py-2 leading-8 text-gray-800'> looking for a yab</p>
+            <p className='text-center text-md py-2 leading-8 text-gray-800'>I know how to code</p>
           </div>
-          <div>
+          <div className=' lg:flex gap-10 justify-between'>
+            <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+              <Image src={weather} alt="" className=" shadow-lg"></Image>
+              <h3 className='text-lg font-medium pt-8 pb-2'>Weather Dashborad</h3>
+            </div>
+            <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+              <Image src={passwordgen} alt="" className=" shadow-lg"></Image>
+              <h3 className='text-lg font-medium pt-8 pb-2'>Password Generator</h3>
+            </div>
+            <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+              <Image src={teamgen} alt="" className=" shadow-lg"></Image>
+              <h3 className='text-lg font-medium pt-8 pb-2'>Team Generator 'Node.js'</h3>
+            </div>
 
           </div>
         </section>
         
       </main>
     </>
+    </div>
   )
 }
